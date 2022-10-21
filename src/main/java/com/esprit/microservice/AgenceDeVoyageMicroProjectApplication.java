8d350990cc5d1c5c029e6593255988269bf7f93f
Fourat888 <fourat.anane@esprit.tn>
2022-10-21 17:29:31 +0200

    addfacture

--- src/main/java/com/esprit/microservice/AgenceDeVoyageMicroProjectApplication.java
+++ src/main/java/com/esprit/microservice/AgenceDeVoyageMicroProjectApplication.java
@@ -1,7 +1,14 @@
 package com.esprit.microservice;
 
+import java.util.stream.Stream;
+
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.esprit.microservice.Entity.Facture;
+import com.esprit.microservice.Repository.FactureRepository;
 
 @SpringBootApplication
 public class AgenceDeVoyageMicroProjectApplication {
@@ -10,4 +17,14 @@ public class AgenceDeVoyageMicroProjectApplication {
 		SpringApplication.run(AgenceDeVoyageMicroProjectApplication.class, args);
 	}
 
+	
+	@Bean
+	 ApplicationRunner init(FactureRepository repository){
+		return args ->{
+			Stream.of("Movenpick","Regency","hammamet").forEach(nom ->{
+				repository.save(new Facture(nom));
+			});
+			repository.findAll().forEach(System.out::println);
+		};
+	}
 }
